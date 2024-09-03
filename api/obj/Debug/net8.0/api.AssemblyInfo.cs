314ed65037d85f364ec03daf9431ac38854c8b65
Saily Hurtado <saily.hurtado@ntsprint.com>
2024-09-03 12:27:29 +0200

    Adding Entity Framework. Relation One-To-Many.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+829d1537ac055269d4a362628da3769bb48d7e7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d418c14efd20dcc696cba2d87c0a991d97ebe9b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
