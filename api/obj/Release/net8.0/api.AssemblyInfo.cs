52f99f9551986923af9a73d3d309f5403c3c4c14
Saily Hurtado <saily.hurtado@ntsprint.com>
2024-09-05 09:46:10 +0200

    Adding to my To-Many a CREATE (Post).

--- api/obj/Release/net8.0/api.AssemblyInfo.cs
+++ api/obj/Release/net8.0/api.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001f5a672c41761b8326bdd6f830fc5cecd14749")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
