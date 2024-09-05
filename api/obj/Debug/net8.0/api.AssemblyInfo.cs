dc2134a307498ab9815045e85c69f3d3a7e46db0
Saily Hurtado <saily.hurtado@ntsprint.com>
2024-09-05 14:32:24 +0200

    Installing Identity. Installing Nuget Packages: - Microsoft.Extensions.Identity.Core - Microsoft.AspNetCore.Identity.EntityFrameworkCore - Microsoft.AspNetCore.Authentication.JwtBearer

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19d52336c9b3ad12a7eb6ce4481e306fc6afd4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67117ab484bd625ef9f54a0fcce8fd065701569c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
